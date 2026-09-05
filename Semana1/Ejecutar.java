bd439acdb780485fc115c03b2796034c2015c7d8
alejandro <259216818+alejandroxd137@users.noreply.github.com>
2026-09-05 01:44:08 +0000

    ejemplos en java

---
+++ Semana1/Ejecutar.java
@@ -0,0 +1,26 @@
+public class Ejecutar {
+
+    public static void main(String[] args) {
+       
+
+       int[] a = {5, 16, 8, 9, 10, 25, 19, 3, 6, 22, 2 };
+
+       for (int i = 0; i < a.length; i++) {
+           System.out.println(a[i]);
+       }
+
+       int suma = 0;
+         for (int i = 0; i < a.length; i++) {
+            suma += a[i];
+         }
+         System.out.println("La suma de los elementos del arreglo a: " + suma);
+
+
+         int[] b = new int[11];
+
+         for (int i = 0; i < a.length; i++) {
+             b[i] (Math.radom( ) * (max - min - 1) + min );
+         }
+    }
+
+}
