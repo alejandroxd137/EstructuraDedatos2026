6227a6cad36d75a54a290a79332c0131c655cfc7
alejandro <259216818+alejandroxd137@users.noreply.github.com>
2026-09-19 01:49:51 +0000

    clasesemana3

---
+++ Semana2/Arreglodenumerospareimparesdelarrglos7.java
@@ -0,0 +1,28 @@
+package Semana2;
+
+public class Arreglodenumerospareimparesdelarrglos7 {
+     public static void main(String[] args) {
+        int[] arreglo = { 8, 5, 3, 10, 2, 8, 1 };
+        int[] arregloPar = new int[arreglo.length / 2];
+        int[] arregloImpar = new int[arreglo.length / 2 + 1];
+
+        if (arreglo.length % 2 == 0) {
+            for (int i = 0; i < arreglo.length / 2; i++) {
+                arregloPar[i] = arreglo[i] + arreglo[arreglo.length - 1 - i];
+            }
+            System.out.println("Arreglo Par: ");
+            for (int num : arregloPar) {
+                System.out.print(num + " ");
+            }
+        } else {
+            for (int i = 0; i < arreglo.length / 2; i++) {
+                arregloImpar[i] = arreglo[i] + arreglo[arreglo.length - 1 - i];
+            }
+            arregloImpar[arregloImpar.length - 1] = arreglo[arreglo.length / 2];
+            System.out.println("Arreglo Impar: ");
+            for (int num : arregloImpar) {
+                System.out.print(num + " ");
+            }
+        }
+}
+}
---
+++ Semana3/OperacionesMatrices.java
@@ -0,0 +1,43 @@
+public class OperacionesMatrices {
+  public int[][] llenarMatriz(int f, int c, int min, int max){
+        int[][] m = new int[f][c]; //Declarando la matriz
+        for(int i = 0; i < m.length; i++){
+            for(int j = 0; j < m[0].length; j++){
+                m[i][j] = (int)(Math.random() * (max - min - 1 ) + min);
+            }
+        }
+        return m;
+    }
+    
+    //Método que permite mostrar una matriz de enteros
+    public String imprimirMatriz(int[][] m){
+        String cad = "";
+        for(int i = 0; i < m.length; i++){
+            cad += "|";
+            for(int j = 0; j < m[0].length; j++){
+                cad+= m[i][j] + "|";
+            }
+            cad+= "\n"; //Salto de línea --> \n
+        }
+        return cad;
+    }
+    
+    //Método para sumar las filas y columnas de la matriz
+    public String sumaFilasColumnas(int[][] m){
+        String cad = "";
+        int sumaFilas; 
+        int sumaColumnas;
+
+        for(int i = 0; i < m.length; i++){
+            sumaFilas = 0;
+            sumaColumnas = 0;
+            for(int j = 0; j < m[0].length; j++){
+                sumaFilas += m[i][j];
+                sumaColumnas += m[j][i];
+            }
+            cad += "sumaFila " + i + " " + sumaFilas + "\n";
+            cad += "sumaColumna " + i + " " + sumaColumnas + "\n";
+        }        
+        return cad;
+    }
+}
